eccca9bdbc6c72fb0f0168d75617d085fb428836
CyskoFR <franc.sko91@gmail.com>
2022-04-28 16:21:33 +0200

    structure

--- js/main.js
+++ js/main.js
@@ -5,9 +5,27 @@
 //   va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+let km = document.getElementById("n-chilometri").innerHTML * 1;
+
+const prezzoBigliettoAlKm =  km * 0.21;
+
+const scontoMinorenne = prezzoBigliettoAlKm * 20 / 100;
+
+const scontoOver65 = prezzoBigliettoAlKm * 40 / 100;
+
 function onlyOne(checkbox) {
-    var checkboxes = document.getElementsByName('check')
+    let checkboxes = document.getElementsByName('check')
     checkboxes.forEach((item) => {
         if (item !== checkbox) item.checked = false
     })
+}
+
+function promptMe() {
+    if (document.getElementById("minorenne").checked) {
+        document.getElementById("price-box").innerHTML = prezzoBigliettoAlKm - scontoMinorenne;
+    } else if (document.getElementById("over65").checked) {
+        document.getElementById("price-box").innerHTML = prezzoBigliettoAlKm - scontoOver65;
+    } else {
+        document.getElementById("price-box").innerHTML = prezzoBigliettoAlKm;
+    }
 }
